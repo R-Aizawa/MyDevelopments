28c4dde1cf53209885a928aa9abad3b9e7069d90
R-Aizawa <aizawacchi@gmail.com>
2015-09-16 18:07:34 +0900

    fuel環境作成

---
+++ fuel/app/logs/2015/09/16.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2015-09-16 07:42:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-09-16 07:43:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+INFO - 2015-09-16 15:27:35 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = ""
+INFO - 2015-09-16 15:27:35 --> Fuel\Core\Request::execute - Called
+INFO - 2015-09-16 15:27:35 --> Fuel\Core\Request::execute - Setting main Request
